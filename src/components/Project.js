5245c6a16420ddd31e83929ab9c3c551dad4e7f1
sevaggap <sevag.gaprielian@gmail.com>
2022-03-29 14:36:36 -0400

    removed description from cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -17,7 +17,6 @@ function Project({project}) {
     <img src={project.img} style={styles.img} class="card-img-top" alt="..."/>
     <div class="card-body">
     <h5 class="card-title">{project.name}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href={project.github} class="card-link btn btn-primary">Github</a>
     <a href={project.deployedLink !== null ? project.deployedLink : project.walkThrough} class="card-link btn btn-primary">{project.deployedLink !== null ? 'Deployed Application' : 'Walkthrough Video'}</a>
   </div>
